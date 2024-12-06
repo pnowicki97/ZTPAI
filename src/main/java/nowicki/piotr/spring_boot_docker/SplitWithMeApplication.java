cb489d7acf08ff0d313e966c6b523d937a2b634b
pnowicki_235078 <235078@student.pwr.edu.pl>
2024-12-06 17:58:22 +0100

    Create Spring project

---
+++ src/main/java/nowicki/piotr/spring_boot_docker/SplitWithMeApplication.java
@@ -0,0 +1,13 @@
+package nowicki.piotr.spring_boot_docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitWithMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplitWithMeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nowicki/piotr/spring_boot_docker/SplitWithMeApplicationTests.java
@@ -0,0 +1,13 @@
+package nowicki.piotr.spring_boot_docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitWithMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
